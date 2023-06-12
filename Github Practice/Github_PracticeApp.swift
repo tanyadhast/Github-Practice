7d2efdc513e306c985eb3955618c6c2d11272830
tanya <tanya.dhast@gmail.com>
2023-06-12 08:24:13 -0700

    Initial Commit

---
+++ Github Practice/Github_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_PracticeApp.swift
+//  Github Practice
+//
+//  Created by Dilan Hasthantra on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Github_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
